b9acbd7b0ef8fc7de57d961ad523567c153f41c7
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-12-14 22:01:56 +0100

    added Stripe API

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import PaymentForm from "./components/PaymentForm.tsx";
 
-const stripePromise = loadStripe("YOUR_STRIPE_PUBLIC_KEY");
+const stripePromise = loadStripe(
+  "pk_test_51ONJT9BtUF6TF1sVqTrvV2wSIvtiKOzzBvXzctr2YgdLXgoKAjH2ZHrE3XC8SfPo1NAlcqLAnirjuzrdoZTiLsh20010odktBe"
+);
 
 const App: React.FC = () => {
   return (
